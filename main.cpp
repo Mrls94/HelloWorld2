f765d03c04caec44697802cbc7b629d05472c085
NoDamnH <torres.jeff@hotmail.com>
2015-05-27 22:11:29 -0500

    First build of the super Hello World Project
    we gonna pwn some ass

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void PrintSomething()
+{
+	std::cout << "Something" << std::endl;
+}
+
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+
+	PrintSomething();
+
+	return 0;
+}
